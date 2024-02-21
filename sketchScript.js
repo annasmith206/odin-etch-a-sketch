e61ff6b9a7ed4219083f8fd2ae21b95b8de81161
Anna <aksmithnw@gmail.com>
2024-02-21 13:04:13 -0800

    Create 16x16 grid of divs
    Initialize index.html with a container div, styles.css with
    style for row and squares, and script with function to
    create 16x16 grid of square divs.

---
+++ sketchScript.js
@@ -0,0 +1,23 @@
+const gridContainer = document.querySelector(".container");
+
+let grid = createGrid();
+
+function createGrid(){
+    let grid = [];
+    for (let i = 0; i < 16; i++){
+        let row = [];
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+
+        for (let i = 0; i < 16; i++){
+            let squareDiv = document.createElement("div");
+            squareDiv.classList.add("square");
+            row.push(squareDiv);
+            rowDiv.appendChild(squareDiv);
+        }
+
+        grid.push(row);
+        gridContainer.appendChild(rowDiv);
+    }
+    return grid;
+}
